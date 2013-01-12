560887ab0d823188f17a2b11abeb3370eb64dd33
chausler <chausler@gmail.com>
2013-01-12 16:02:51 +0100

    adjusted

--- deep/test/test_tadbn.py
+++ deep/test/test_tadbn.py
@@ -10,19 +10,19 @@ import theano
 test_data = np.array([np.sin(np.arange(400) * 0.2),
                       np.sin(np.arange(400) * 0.4)]).T
 
-print test_data.shape
+
 batchdata = numpy.asarray(test_data, dtype=theano.config.floatX)
 
-delay = 10
+delay = 5
 
 numpy_rng = numpy.random.RandomState(123)
 n_dim = [test_data.shape[1]]
 
 dbn_tadbn = TADBN(numpy_rng=numpy_rng, n_ins=[n_dim],
           hidden_layers_sizes=[100],
-          n_outs=0, sparse=0.0, delay=delay, learning_rate=0.01)
+          sparse=0.0, delay=delay, learning_rate=0.01)
 
-dbn_tadbn.pretrain(batchdata, plot_interval=5, static_epochs=30,
+dbn_tadbn.pretrain(batchdata, plot_interval=5, static_epochs=1,
                    save_interval=10, epochs=30, all_epochs=60,
                    batch_size=5)
 
